4b2d8b7f39194171fa71d8e708609f7184bf389b
R. A. Borrelli <borrelli@localhost.localdomain>
2019-06-17 16:40:34 -0600

    air graphs

--- src/igem/neutronics/air/bare/air-backfill/5wt/plot_all.in.one_cask.thickness_dose.rate_l3030_east.py
+++ src/igem/neutronics/air/bare/air-backfill/5wt/plot_all.in.one_cask.thickness_dose.rate_l3030_east.py
@@ -158,15 +158,15 @@ legend_font=42
 # position of the annotation dependent on axis domain and range
 #
 annotate_title='L-3030'
-annotate_x=25
+annotate_x=23
 annotate_y=2700
 #
 annotate_title2='Air backfill'
-annotate_x2=25
+annotate_x2=23
 annotate_y2=1700
 #
 annotate_title3='5 wt% $^{10}B$'
-annotate_x3=25
+annotate_x3=23
 annotate_y3=900
 #
 ###
